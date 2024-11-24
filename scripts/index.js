5f025b2c6a97370fbe954cb9ed5c5927c2555b2e
Ilya Lyudevig <lyudevig@gmail.com>
2024-11-24 21:05:43 +0200

    Add card template and populating from initialCards array, add profile info save button functionality

--- scripts/index.js
+++ scripts/index.js
@@ -25,15 +25,50 @@ let initialCards = [
   },
 ];
 
-let editProfileButton = document.querySelector(".profile__edit-button");
-let closeEditProfileFormButton = document.querySelector(".modal__close-button");
+const editProfileButton = document.querySelector(".profile__edit-button");
+const closeEditProfileFormButton = document.querySelector(
+  ".modal__close-button"
+);
+const editProfileFormSaveButton = document.querySelector(".form__save-button");
+const profileFormElement = document.querySelector(".modal__form");
+const nameInput = profileFormElement.querySelectorAll(".form__input")[0];
+const jobInput = profileFormElement.querySelectorAll(".form__input")[1];
+const profileName = document.querySelector(".profile__name");
+const profileJob = document.querySelector(".profile__job");
+const placesList = document.querySelector(".places__list");
 function editProfile() {
   document.querySelector(".modal").classList.add("modal_opened");
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
 }
 
 function closeEditProfileForm() {
   document.querySelector(".modal").classList.remove("modal_opened");
 }
 
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+}
+
+function getCardElement(data) {
+  const cardElement = placesList
+    .querySelector("#card-template")
+    .content.cloneNode(true);
+  const cardTitle = cardElement.querySelector(".card__title");
+  const cardImage = cardElement.querySelector(".card__image");
+
+  cardTitle.textContent = data.name;
+  cardImage.setAttribute("src", data.link);
+
+  return cardElement;
+}
+
+for (let card of initialCards) {
+  placesList.append(getCardElement(card));
+}
+
 editProfileButton.addEventListener("click", editProfile);
 closeEditProfileFormButton.addEventListener("click", closeEditProfileForm);
+profileFormElement.addEventListener("submit", handleProfileFormSubmit);
